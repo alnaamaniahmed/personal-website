b35408524b8043b54d0db2274d2ad247efc46b36
alnaamani <ah666052@dal.ca>
2024-04-01 13:23:37 -0300

    Added the homepage welcomeText

--- personal-portfolio/src/pages/HomePage.jsx
+++ personal-portfolio/src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import '../homePage.css';
+import newpic from '../newpic.png';
 
 
 function HomePage(props){
-    return (<h1 style={{color: "white"}}>Home Page</h1>);
+    return (
+    <div className='biggerGrid'>
+        <div className='welcomeGrid'>
+            <div className='text-1'>
+                Hey There!
+            </div>
+            <div className='text-2'>
+                I'M <span id="name">Ahmed Al-Naamani</span>
+            </div>
+            <div className='text-3'>
+                Full Stack Web Developer
+            </div>
+        </div>
+        <div className="ikbalImage">
+                <img src={newpic} alt="Ikbal" />
+            </div>
+    </div>
+        
+    );
 }
 export default HomePage;
