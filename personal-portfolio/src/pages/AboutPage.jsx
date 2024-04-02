8ea8dcd623cfed4b44ba83ec52305cee6dcb14f2
alnaamani <ah666052@dal.ca>
2024-04-02 09:00:10 -0300

    started working on the AboutPage halfway through it

--- personal-portfolio/src/pages/AboutPage.jsx
+++ personal-portfolio/src/pages/AboutPage.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import thirdPic from '../thirdPic.png';
+import '../aboutPage.css';
+import dumbbellIcon from '../icons/dumbbell.png';
+import iceSkates from '../icons/iceSkates.png';
 
 
 function AboutPage(props){
-    return (<h1 style={{color: "white"}}>AboutPage</h1>);
+    return (
+        <div className='gridAbout'>
+            <div className='ikbalImage3'>
+                <img src={thirdPic} alt='Ikbal' />
+            </div>
+            <div className='words'>
+                <h1 className='headText'>Know Who I am</h1>
+                <p id='firstText'>I am from the culturally rich and scenic city of Muscat, Oman. As a Junior Computer Science student at Dalhousie University. I am passionately coding my path through the complexities of technology.</p>
+                <p id='secondText'>In addition to my academic pursuits, I engage in various activities to maintain a balanced lifestyle!</p>
+                <ul className='activities'>
+                    <li className='activity-1'>Lifting <span className='dumbbell'><img src={dumbbellIcon} alt='dumbbell'/></span></li>
+                    <li className='activity-2'>Ice skating <span className='ice'> <img src={iceSkates} alt='iceSkates' className='ice'/></span></li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 export default AboutPage;
