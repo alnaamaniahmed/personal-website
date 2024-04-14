7451a00f355ed7951c8b65ef379837282f0c9f4a
alnaamani <ah666052@dal.ca>
2024-04-14 17:31:29 -0300

    Completed the projectPage

--- personal-portfolio/src/pages/ProjectsPage.jsx
+++ personal-portfolio/src/pages/ProjectsPage.jsx
@@ -16,34 +16,46 @@ function ProjectsPage(props){
         <div className='cardGrid'>
             <div className='card'>
                 <div className='cardFlexCont'>
-                    <img src={personalWebProject}/>
-                    <h2>Personal <span className='projectSpan'>Website</span></h2>
+                    <img className='cardImage' src={personalWebProject}/>
+                    <h2 className='cardText'>Personal <span className='projectSpan'>Website</span></h2>
                     <p className='cardPara'>Designed and developed a personal website using React.js, HTML, and CSS, showcasing a portfolio of projects and skills. The site features responsive design and interactive elements, effectively demonstrating proficiency in modern web development practices.</p>
                     <button className='githubButton' onClick={() => window.open('https://www.github.com/alnaamaniahmed/personal-website', '_blank')}>
-                        <GitHubIcon className='smth'/>
-                        GitHub
+                        <GitHubIcon className='iconButton'/>
+                        <p className='textButton'>GitHub</p>
                     </button>
                 </div>
             </div>
             <div className='card'>
                 <div className='cardFlexCont'>
-                        <img src={quickCash}/>
-                        <h2>Quick <span className='projectSpan'>Cash</span></h2>
+                        <img className='cardImage' src={quickCash}/>
+                        <h2 className='cardText'>Quick <span className='projectSpan'>Cash</span></h2>
                         <p className='cardPara'>Collaborated on developing QuickCash, an Android job search app, using Agile methods. Integrated Firebase for secure and real-time data handling.</p>
+                        <button className='githubButton' onClick={() => window.open('https://github.com/alnaamaniahmed/quickCash', '_blank')}>
+                            <GitHubIcon className='iconButton'/>
+                            <p className='textButton'>GitHub</p>
+                        </button>
                 </div>
             </div>
             <div className='card'>
                 <div className='cardFlexCont'>
-                        <img src={simonGame}/>
-                        <h2>Simon <span className='projectSpan'>Game</span></h2>
+                        <img className='cardImage'src={simonGame}/>
+                        <h2 className='cardText'>Simon <span className='projectSpan'>Game</span></h2>
                         <p className='cardPara'>A dynamic version of the Simon Game built with jQuery for interactive play and MVC architecture for clean code structure. Features a responsive design ensuring smooth gameplay on various devices.</p>
+                        <button className='githubButton' onClick={() => window.open('https://github.com/alnaamaniahmed/simonGame', '_blank')}>
+                            <GitHubIcon className='iconButton'/>
+                            <p className='textButton'>GitHub</p>
+                        </button>
                 </div>
             </div>
             <div className='card'>
                 <div className='cardFlexCont'>
-                        <img src={artByYou}/>
-                        <h2>Art By <span className='projectSpan'>You</span></h2>
+                        <img className='cardImage' src={artByYou}/>
+                        <h2 className='cardText'>Art By <span className='projectSpan'>You</span></h2>
                         <p className='cardPara'>Developed a mobile-responsive website with Bootstrap, integrated dynamic content using PHP and MySQL, and managed databases with phpMyAdmin.</p>
+                        <button className='githubButton' onClick={() => window.open('https://github.com/alnaamaniahmed/artbyyou-', '_blank')}>
+                            <GitHubIcon className='iconButton'/>
+                            <p className='textButton'>GitHub</p>
+                        </button>
                 </div>
             </div>
         </div>
