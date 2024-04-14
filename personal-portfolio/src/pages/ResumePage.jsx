58b8d38e460a19b2489be6321954ae96d2480cea
alnaamani <ah666052@dal.ca>
2024-04-14 18:58:17 -0300

    Completed Resume Page

--- personal-portfolio/src/pages/ResumePage.jsx
+++ personal-portfolio/src/pages/ResumePage.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import '../resumePage.css';
+import {ReactComponent as DownloadIcon} from '../icons/download.svg';
+import resumePic from '../icons/resume.png';
 
 
 function ResumePage(props){
-    return (<h1 style={{color: "white"}}>ResumePage</h1>);
+    const handleDownloadClick = () => {
+        window.open('https://ahmedalnaamani.netlify.app/resources/resume.pdf', '_blank');
+    };
+    return(
+        <div className='resumeFlexContainer'>
+            <button className='downloadButton'onClick={handleDownloadClick}>
+                <DownloadIcon className='downloadIcon'/>
+                <p className='textDownload'>Download</p>
+            </button>
+            <img className='resumeImg' src={resumePic} alt='resumeImage'/>
+            <button className='downloadButton' id='secondDownloadButton' onClick={handleDownloadClick}>
+                <DownloadIcon className='downloadIcon'/>
+                <p className='textDownload'>Download</p>
+            </button>
+        </div>
+    );
 }
 export default ResumePage;
